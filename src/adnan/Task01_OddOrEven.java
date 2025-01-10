890bcf3380cc0d29f0a22253ec9d266984e1cd62
Afilya <adnanfilya24@gmail.com>
2025-01-10 20:58:03 +0300

    initial commit for project set up

---
+++ src/adnan/Task01_OddOrEven.java
@@ -0,0 +1,18 @@
+package adnan;
+
+public class Task01_OddOrEven {
+    /*
+Write a method that can identify  a given number is even or odd.
+
+       EX:
+
+              identify(5) --> "Odd"
+              identify(6) --> "Even"
+
+
+     */
+    public static void main(String[] args) {
+
+
+    }
+}
