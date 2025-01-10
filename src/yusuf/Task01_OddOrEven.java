f44cd060d57c9e7b65a2418da6b8a6cea6d71320
Yusuf <yusuf.asik123@gmail.com>
2025-01-10 21:22:55 +0300

    Week1 OddOrEven added.

---
+++ src/yusuf/Task01_OddOrEven.java
@@ -0,0 +1,27 @@
+package yusuf;
+
+public class Task01_OddOrEven {
+    /*
+Write a method that can identify  a given number is even or odd.
+
+       EX:
+
+              identify(5) --> "Odd"
+              identify(6) --> "Even"
+
+
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     *
+     * returns odd or even if number
+     *
+      * @param num
+     */
+    public static void identify(int num){
+
+    }
+}
