04b318bf710edb799c6945f3acd5fd10cb3a89f2
Afilya <adnanfilya24@gmail.com>
2025-01-11 09:21:29 +0300

    Task completed

--- src/adnan/Task01_OddOrEven.java
+++ src/adnan/Task01_OddOrEven.java
@@ -5,14 +5,26 @@ public class Task01_OddOrEven {
 Write a method that can identify  a given number is even or odd.
 
        EX:
-
               identify(5) --> "Odd"
               identify(6) --> "Even"
-
-
      */
     public static void main(String[] args) {
 
+        identify(55);
+        identify(66);
+        identify(0);
+        identify(-1);
+
+    }
+
+    public static void identify(int num) {
+
+        /**
+         * Checks if the given number odd or even
+         */
+
+        String result = num % 2 == 0 ? "even" : "odd";
+        System.out.println(result);
 
     }
 }
